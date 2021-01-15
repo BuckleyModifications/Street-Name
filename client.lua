cbb53494798ccf7f82004db68b781a6729d102ab
BuckleyModifications <77497482+BuckleyModifications@users.noreply.github.com>
2021-01-15 14:34:31 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,18 @@
+--- The hashes are in the folder called gta 5 hashes.txt 
+	
+	--- Make sure that you go to a road in gta 5 then get the name of the road. Then go to the txt and then CTRL + F and search the name.
+	
+		--- It will then come up with the hash and you put it there.
+		
+			--- Once you have done that you will see the part CHANGE ME. Change that to the name of the road you would like it to be displayed as in game.
+				
+				--- At the ENTER HASH, DO NOT put speechmarks just leave it blank
+
+print ("Successfully loaded Road Name Editor by Buckley Modifications")
+
+AddTextEntryByHash(hash, 'CHANGE ME')
+AddTextEntryByHash(hash, 'CHANGE ME')
+AddTextEntryByHash(hash, 'CHANGE ME')
+AddTextEntryByHash(hash, 'CHANGE ME')
+AddTextEntryByHash(hash, 'CHANGE ME')
+AddTextEntryByHash(hash, 'CHANGE ME')
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Buckley Modifications'
+description 'This resource makes it so when you want to change the name of a road in-game you can!'
+version '0.0.1'
+
+client_scripts {
+    'client.lua'
+}
